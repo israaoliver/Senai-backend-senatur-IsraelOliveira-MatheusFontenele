350712b2d67be12f99da2ac1b934e0a82562f4b0
israaoliver <isrraaoliveira@gmail.com>
2020-03-10 17:20:08 -0300

    Pacotes

---
+++ Senai.Senatur.WebApi/Senai.Senatur.WebApi/Interfaces/IPacoteRepository.cs
@@ -0,0 +1,27 @@
+﻿using Senai.Senatur.WebApi.Domains;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Senai.Senatur.WebApi.Interfaces
+{
+    interface IPacoteRepository
+    {
+        List<Pacotes> List();
+
+        Pacotes BuscarId(int id);
+
+        void Atualizar(Pacotes pct);
+
+        void Cadastrar(Pacotes pct);
+
+        IEnumerable<Pacotes> PacotesAtivos();
+
+        IEnumerable<Pacotes> PacotesInativos();
+
+        IEnumerable<Pacotes> PorCidade(string cidade);
+
+        IEnumerable<Pacotes> PorPreco(string ordem);
+    }
+}
---
+++ Senai.Senatur.WebApi/Senai.Senatur.WebApi/Repositories/PacoteRepository.cs
@@ -0,0 +1,67 @@
+﻿using Senai.Senatur.WebApi.Domains;
+using Senai.Senatur.WebApi.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Senai.Senatur.WebApi.Repositories
+{
+    public class PacoteRepository : IPacoteRepository
+    {
+        SenaturContext ctx = new SenaturContext();
+
+        public void Atualizar(Pacotes pct)
+        {
+            var PacoteAchado = ctx.Pacotes.Find(pct.IdPacote);
+
+            if(PacoteAchado != null)
+            {
+                if(pct.NomePacote != null)
+                {
+                    
+                }
+
+            
+            }
+        }
+
+        public Pacotes BuscarId(int id)
+        {
+
+            return ctx.Pacotes.FirstOrDefault(p => p.IdPacote == id);
+
+        }
+
+        public void Cadastrar(Pacotes pct)
+        {
+            ctx.Pacotes.Add(pct);
+            ctx.SaveChanges();
+        }
+
+        public List<Pacotes> List()
+        {
+            return ctx.Pacotes.ToList();
+        }
+
+        public IEnumerable<Pacotes> PacotesAtivos()
+        {
+            return ctx.Pacotes.Where(p => p.Ativo == true);
+        }
+
+        public IEnumerable<Pacotes> PacotesInativos()
+        {
+            return ctx.Pacotes.Where(p => p.Ativo == false);
+        }
+
+        public IEnumerable<Pacotes> PorCidade(string cidade)
+        {
+            return ctx.Pacotes.Where(p => p.NomeCidade == cidade);
+        }
+
+        public IEnumerable<Pacotes> PorPreco(string ordem)
+        {
+            return ctx.Pacotes.OrderBy(p => p.Valor);        }
+
+    }
+}
