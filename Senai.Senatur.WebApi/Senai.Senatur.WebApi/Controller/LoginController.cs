e2a34161cff8a49f62d003ba65201dfd39a4bba3
israaoliver <isrraaoliveira@gmail.com>
2020-03-10 14:53:58 -0300

    StartUp

---
+++ Senai.Senatur.WebApi/Senai.Senatur.WebApi/Controller/LoginController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Senai.Senatur.WebApi.Controller
+{
+    [Produces("application/json")]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LoginController : ControllerBase
+    {
+
+
+    }
+}
